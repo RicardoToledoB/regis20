93ea09180d7f530566107fc8b023aa9d903d6a16
Ricardo Toledo <ricardotoledo@Ricardos-MacBook-Pro.local>
2025-12-15 14:35:08 -0300

    kasdkasd

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  base: './', // 👈 importante para servir correctamente los assets en Railway
+  build: {
+    outDir: 'dist', // por defecto, pero explícito
+  },
+  server: {
+    port: 8080,
+    host: '0.0.0.0', // 👈 opcional si quieres levantarlo igual que Railway
   },
 })
